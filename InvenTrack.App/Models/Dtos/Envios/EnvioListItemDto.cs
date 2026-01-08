54b5c2273d33db434cd2642f75644c3f68197ba8
GasconiXx <agasconcrespo@gmail.com>
2026-01-08 09:37:02 +0100

    dtos para consultas

---
+++ InvenTrack.App/Models/Dtos/Envios/EnvioListItemDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventrack.App.Models.Dtos.Envios
+{
+    public sealed class EnvioListItemDto
+    {
+        public int EnvioId { get; set; }
+        public int PaqueteId { get; set; }
+        public string CodigoSeguimiento { get; set; } = "";
+        public string Estado { get; set; } = "";
+        public int? IntentosEntrega { get; set; }
+        public DateTime? FechaSalida { get; set; }
+        public DateTime? FechaEntrega { get; set; }
+        public string DireccionDestino { get; set; } = "";
+    }
+}
---
+++ InvenTrack.App/Models/Dtos/Users/UsersDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventrack.App.Models.Dtos.Users
+{
+    public sealed class UsersDto
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Telefono { get; set; } = "";
+        public int RolId { get; set; }
+        public bool? Activo { get; set; }
+        public int? AlmacenId { get; set; }
+    }
+}
